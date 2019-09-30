1f8e5a3a669a0924f81a24592f2d2eb013d9fd2a
Gobinda Das <gobinda168@gmail.com>
2019-09-30 21:21:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 20 }}>
+      <Contact />
+      <Header title="Contact Manager" />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = props => {
+  return (
+    <div>
+      <h1>John Doe</h1>
+      <ul>
+        <li>Email: gobinda168@gamil.com</li>
+        <li>Phone: +91-9957507223</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
